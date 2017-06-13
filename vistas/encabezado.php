7b3755cece422367b18faa11c441361e18cf5ac5
esteban1234 <tva_libra@hotmail.com>
2017-06-13 13:32:50 -0500

    metas

--- vistas/encabezado.php
+++ vistas/encabezado.php
@@ -2,12 +2,12 @@
 <html lang="en">
 <head>
 	<meta charset="UTF-8">
+	<meta name="google-site-verification" content="KzHZ48WgGkVflwNPv2ez_Jy3gzvc7ZlD5SK4fQ2RbLc" />
 	<link rel="icon" type="image/png" href="img/favicon.png" />
-
 	<title>CONSTRUCTORA CONSEDIF</title>
 	 <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0"> 
   	<META NAME="Title" content="CONSTRUCTORA CONSEDIF">
-  	<META NAME="Author" CONTENT="[]"/>
+  	<META NAME="Author" CONTENT="[http://consedif.com.mx/]"/>
   	<META name="Description" content="Somos una compañía ubicada en la ciudad de TUXTLA GUTIERREZ, CHIAPAS, México, dedicados a ofrecer servicios Desarrollo de proyectos de construcción, Diseño de planos, remodelación, interpretación de planos, levantamientos y ampliaciones."/>
 	 <META NAME="Keywords" CONTENT="Constructora de casas TUXTLA GUTIERREZ, CHIAPAS, Constructora TUXTLA GUTIERREZ, CHIAPAS, Contructora, CONSEDIF TUXTLA GUTIERREZ, CHIAPAS, CONSEDIF TUXTLA GUTIERREZ, CHIAPAS, remodelación de casas TUXTLA GUTIERREZ, CHIAPAS, ampliaciones de casa TUXTLA GUTIERREZ, CHIAPAS, ingenieria civil TUXTLA GUTIERREZ, CHIAPAS, diseño de planos, levantamientos, mexico TUXTLA GUTIERREZ, CHIAPAS, desarrollo de proyectos de construcción"/>
   	<link rel="stylesheet" href="css/bootstrap.css">
